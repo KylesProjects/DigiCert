17bebfd0d3cc548a4e949d04015817368f3f4f84
KyleBlakeHarmon <kashout15@gmail.com>
2021-09-17 10:45:14 -0700

    Submitting SpringBoot REST API.

--- DigiCert/src/main/java/com/digicert/DigiCertApplication.java
+++ DigiCert/src/main/java/com/digicert/DigiCertApplication.java
@@ -3,7 +3,7 @@ package com.digicert;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// This class enables the auto-configuration of the project and scans all included components. 
+//This class enables the auto-configuration of the project and scans all included components. 
 @SpringBootApplication
 public class DigiCertApplication {
 
